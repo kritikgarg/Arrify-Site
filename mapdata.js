e87db6ced0edec1b92af2fbd01e0ffce52855588
kritikgarg <kritikgarg240194@gmail.com>
2022-10-29 17:33:37 +0530

    New Updats

---
+++ mapdata.js
@@ -0,0 +1,440 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "650", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    
+    //Label defaults
+    label_color: "#002767",
+    hide_labels: "no",
+    border_color: "white",
+    
+    //State defaults
+    state_description: "Learn about the products available in your state.",
+    state_color: "#77bcff",
+    state_hover_color: "#3c3876",
+    state_url: "http://simplemaps.com",
+    all_states_inactive: "no",
+    
+    //Location defaults
+    location_description: "Field Office",
+    location_color: "#ff46a2",
+    location_pulse: "yes",
+    location_pulse_size: "3",
+    location_opacity: ".8",
+    location_url: "http://simplemaps.com",
+    all_states_zoomable: "yes",
+    location_size: "40",
+    location_type: "marker",
+    all_locations_inactive: "no",
+    url_new_tab: "yes",
+    auto_load: "yes",
+   
+    //Zoom settings
+    zoom: "yes",
+    initial_zoom: "-1",
+    initial_zoom_solo: "yes",
+    
+    //Advanced settings
+    div: "map",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    label_hover_color: "",
+    label_size: "",
+    label_font: "",
+    popups: "detect"
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/hawaii/",
+      description: "Find Salesforce Developer in Hawaii"
+    },
+    AK: {
+      name: "Alaska",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/alaska/",
+      description: "Find Salesforce Developer in Alaska"
+    },
+    FL: {
+      name: "Florida",
+      inactive: "no",
+      hide: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/florida/",
+      description: "Find Salesforce Developer in Florida"
+    },
+    NH: {
+      name: "New Hampshire",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/new-hampshire/",
+      description: "Find Salesforce Developer in New-Hampshire"
+    },
+    VT: {
+      name: "Vermont",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/vermont/",
+      description: "Find Salesforce Developer in Vermont"
+    },
+    ME: {
+      name: "Maine",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/maine/",
+      description: "Find Salesforce Developer in Maine"
+    },
+    RI: {
+      name: "Rhode Island",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/rhode-island/",
+      description: "Find Salesforce Developer in Rhode-Island"
+    },
+    NY: {
+      name: "New York",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/new-york/",
+      description: "Find Salesforce Developer in New-York"
+    },
+    PA: {
+      name: "Pennsylvania",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/pennsylvania/",
+      description: "Find Salesforce Developer in Pennsylvania"
+    },
+    NJ: {
+      name: "New Jersey",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/new-jersey/",
+      description: "Find Salesforce Developer in New-Jersey"
+    },
+    DE: {
+      name: "Delaware",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/delaware/",
+      description: "Find Salesforce Developer in Delaware"
+    },
+    MD: {
+      name: "Maryland",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/maryland/",
+      description: "Find Salesforce Developer in Maryland"
+    },
+    VA: {
+      name: "Virginia",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/virginia/",
+      description: "Find Salesforce Developer in Virginia"
+    },
+    WV: {
+      name: "West Virginia",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/west-virginia/",
+      description: "Find Salesforce Developer in West-Virginia"
+    },
+    OH: {
+      name: "Ohio",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/ohio/",
+      description: "Find Salesforce Developer in Ohio"
+    },
+    IN: {
+      name: "Indiana",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/indiana/",
+      description: "Find Salesforce Developer in Indiana"
+    },
+    IL: {
+      name: "Illinois",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/illinois/",
+      description: "Find Salesforce Developer in Illinois"
+    },
+    CT: {
+      name: "Connecticut",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/connecticut/",
+      description: "Find Salesforce Developer in Connecticut"
+    },
+    WI: {
+      name: "Wisconsin",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/wisconsin/",
+      description: "Find Salesforce Developer in Wisconsin"
+    },
+    NC: {
+      name: "North Carolina",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/north-carolina/",
+      description: "Find Salesforce Developer in North-Carolina"
+    },
+    DC: {
+      name: "District of Columbia",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/district-of-columbia/",
+      description: "Find Salesforce Developer in District-of-Columbia"
+    },
+    MA: {
+      name: "Massachusetts",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/massachusetts/",
+      description: "Find Salesforce Developer in Massachusetts"
+    },
+    TN: {
+      name: "Tennessee",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/tennessee/",
+      description: "Find Salesforce Developer in Tennessee"
+    },
+    AR: {
+      name: "Arkansas",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/arkansas/",
+      description: "Find Salesforce Developer in Arkansas"
+    },
+    MO: {
+      name: "Missouri",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/missouri/",
+      description: "Find Salesforce Developer in Missouri"
+    },
+    GA: {
+      name: "Georgia",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/georgia/",
+      description: "Find Salesforce Developer in Georgia"
+    },
+    SC: {
+      name: "South Carolina",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/south-carolina/",
+      description: "Find Salesforce Developer in South-Carolina"
+    },
+    KY: {
+      name: "Kentucky",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/kentucky/",
+      description: "Find Salesforce Developer in Kentucky"
+    },
+    AL: {
+      name: "Alabama",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/alabama/",
+      description: "Find Salesforce Developer in Alabama"
+    },
+    LA: {
+      name: "Louisiana",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/louisiana/",
+      description: "Find Salesforce Developer in Louisiana"
+    },
+    MS: {
+      name: "Mississippi",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/mississippi/",
+      description: "Find Salesforce Developer in Mississippi"
+    },
+    IA: {
+      name: "Iowa",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/iowa/",
+      description: "Find Salesforce Developer in Iowa"
+    },
+    MN: {
+      name: "Minnesota",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/minnesota/",
+      description: "Find Salesforce Developer in Minnesota"
+    },
+    OK: {
+      name: "Oklahoma",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/oklahoma/",
+      description: "Find Salesforce Developer in Oklahoma"
+    },
+    TX: {
+      name: "Texas",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/texas/",
+      description: "Find Salesforce Developer in Texas"
+    },
+    NM: {
+      name: "New Mexico",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/new-mexico/",
+      description: "Find Salesforce Developer in New-Mexico"
+    },
+    KS: {
+      name: "Kansas",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/kansas/",
+      description: "Find Salesforce Developer in Kansas"
+    },
+    NE: {
+      name: "Nebraska",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/nebraska/",
+      description: "Find Salesforce Developer in Nebraska"
+    },
+    SD: {
+      name: "South Dakota",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/south-dakota/",
+      description: "Find Salesforce Developer in South-Dakota"
+    },
+    ND: {
+      name: "North Dakota",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/north-dakota/",
+      description: "Find Salesforce Developer in North-Dakota"
+    },
+    WY: {
+      name: "Wyoming",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/wyoming/",
+      description: "Find Salesforce Developer in Wyoming"
+    },
+    MT: {
+      name: "Montana",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/montana/",
+      description: "Find Salesforce Developer in Montana"
+    },
+    CO: {
+      name: "Colorado",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/colorado/",
+      description: "Find Salesforce Developer in Colorado"
+    },
+    UT: {
+      name: "Utah",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/utah/",
+      description: "Find Salesforce Developer in Utah"
+    },
+    AZ: {
+      name: "Arizona",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/arizona/",
+      description: "Find Salesforce Developer in Arizona"
+    },
+    NV: {
+      name: "Nevada",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/nevada/",
+      description: "Find Salesforce Developer in Nevada"
+    },
+    OR: {
+      name: "Oregon",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/oregon/",
+      description: "Find Salesforce Developer in Oregon"
+    },
+    WA: {
+      name: "Washington",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/washington/",
+      description: "Find Salesforce Developer in Washington"
+    },
+    CA: {
+      name: "California",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/california/",
+      description: "Find Salesforce Developer in California"
+    },
+    MI: {
+      name: "Michigan",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/michigan/",
+      description: "Find Salesforce Developer in Michigan"
+    },
+    ID: {
+      name: "Idaho",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/idaho/",
+      description: "Find Salesforce Developer in Idaho"
+    },
+    GU: {
+      name: "Guam",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/guam/",
+      description: "Find Salesforce Developer in Guam"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes",
+      inactive: "yes",
+      description: "Find Salesforce Developer in Virgin Islands"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "no",
+      inactive: "no",
+      url: "https://arrify.com/connect/sfdc-expert/us-s/puerto-rico/",
+      description: "Find Salesforce Developer in Puerto-Rico"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes",
+      inactive: "yes",
+      description: "Find Salesforce Developer in Northern Mariana Islands"
+    }
+  },
+  locations: {},
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
